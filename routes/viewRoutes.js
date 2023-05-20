d403b035f386093c004ace553e3aeb68b5db4afc
Bhushan Sharma <bhushans.sharma86@gmail.com>
2023-05-20 22:32:07 +0530

    stripe webhook changes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
-const bookingController = require('../controllers/bookingController');
+// const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
+
+router.use(viewsController.alerts);
 
 router.get('/me', authController.protect, viewsController.getAccount);
 router.post('/submit-user-data', authController.protect, viewsController.updateUserData);
